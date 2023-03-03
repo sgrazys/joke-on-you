22187bd56af37f37c878cbacae12c81c2ec3a439
sgrazys <s.grazys@gmail.com>
2023-03-03 15:14:31 +0200

    Result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [items, setItems] = useState([]);
 
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					setItems(result);
+				},
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<>
+				<h1 className='title'>
+					🃏Your TOP 10 Jokes after page refresh 🃏
+				</h1>
+				{items.jokes.map((item) => {
+					if (item.type === 'single') {
+						return (
+							<div className='joke-container' key={Math.random()}>
+								<p className='joke'>{item.joke}</p>
+							</div>
+						);
+					} else {
+						return (
+							<div className='joke-container' key={Math.random()}>
+								<p className='joke'>
+									{item.setup} {item.delivery}
+								</p>
+							</div>
+						);
+					}
+				})}
+			</>
+		);
+	}
 }
 
 export default App;
